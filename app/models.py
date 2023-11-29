9510423c3d2b13f1006daf522731c9a23a0a10f7
I-Like-Me <cassgolkin@gmail.com>
2023-11-29 09:47:46 -0500

    all working modules

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.ext.associationproxy import association_proxy
 
 user_char_table = db.Table(
     "user_char_table",
@@ -6,7 +7,8 @@ user_char_table = db.Table(
     db.Column("character_id", db.ForeignKey("character.id"), primary_key=True),
 )
 
-class CharCls(db.model):
+
+class CharCls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     character_id = db.Column("character_id", db.Integer, db.ForeignKey("character.id")),
     cls_5e_id = db.Column("cls_5e_id",db.Integer , db.ForeignKey("cls_5e.id")),
@@ -28,14 +30,14 @@ class User(db.Model):
     def claim_char(self, char_pick):
         if char_pick.npc != True and len(char_pick.player) == 0:
             self.character.append(char_pick)
-        
+
 
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     player = db.relationship("User", secondary=user_char_table, back_populates="character")
     classes_association = db.relationship("CharCls", back_populates="classed_character")
-    classes = db.association_proxy("classes_association", "cls")
+    classes = association_proxy("classes_association", "cls")
     level = db.Column(db.Integer)
     npc = db.Column(db.Boolean)
 
@@ -45,21 +47,13 @@ class Character(db.Model):
     def pick_cls(self, cls_pick):
         self.cls.append(cls_pick)
 
-# class Char_cls_table(db.model):
-#     "char_cls_table",
-#     db.Column("character_id", db.ForeignKey("character.id"), primary_key=True),
-#     db.Column("cls_5e_id", db.ForeignKey("cls_5e.id"), primary_key=True),
-#     cls_level = db.Column(db.Integer)
-#     cls = db.relationship(back_populates='classed_characters')
-#     classed_character = db.relationship(back_populates='classes')
-    
+
 class Cls_5e(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     cls_level = db.Column(db.Integer)
-    # classed_characters = db.relationship(back_populates="cls")
     classed_characters_association = db.relationship("CharCls", back_populates="cls")
-    classed_characters = db.association_proxy('classed_characters_association', 'classed_character')
+    classed_characters = association_proxy('classed_characters_association', 'classed_character')
     def __repr__(self):
         return f'<Cls_5e {self.name}>'
 
