efa2c3be586bd51a8eb14a2dff64bff66cb2853e
I-Like-Me <cassgolkin@gmail.com>
2023-11-30 12:05:44 -0500

    cls picker re added

--- app/models.py
+++ app/models.py
@@ -16,6 +16,10 @@ class CharCls(db.Model):
 
     classed_character = db.relationship("Character", back_populates='cls_association')
     cls = db.relationship("Cls_5e", back_populates='classed_character_association')
+
+    # def lvl_up_cls(self, cls_target):
+    #     for cls
+    #     self.cls_level += 1
    
 
 class User(db.Model):
@@ -46,16 +50,21 @@ class Character(db.Model):
         return f'<Character {self.name}>'
 
     def pick_cls(self, cls_pick):
-        self.cls.append(cls_pick)
-
+        char_cls_ready = CharCls(classed_character=self, cls=cls_pick, cls_level=1)
+        db.session.add(char_cls_ready)
+        db.session.commit()
+        
 
 class Cls_5e(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    cls_level = db.Column(db.Integer)
     classed_character_association = db.relationship("CharCls", back_populates="cls")
     classed_characters = association_proxy('classed_character_association', 'classed_character')
     
     def __repr__(self):
         return f'<Cls_5e {self.name}>'
 
+    def new_cls_adder(cls_name):
+        new_cls = Cls_5e(name=cls_name)
+        db.session.add(new_cls)
+        db.session.commit()
