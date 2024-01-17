f1169adb424290af80dadf7f422e10c288f14af9
I-Like-Me <cassgolkin@gmail.com>
2024-01-17 17:52:36 -0500

    actual traversal of folders without manual looping

--- app/main/toolbox.py
+++ app/main/toolbox.py
@@ -74,17 +74,79 @@ class Builders:
 
     def build_structure(root):
         final_order = []
-        par_list = root.children_dirs
-        chi_list = par_list[0].children_dirs
-        while len(par_list) != 0:
-            print(par_list[0])
-            if len(chi_list) > 0:
-                final_order.append(f"{par_list[0].name}-{chi_list[0].name}")
-                par_list.append(chi_list.pop(0))
-            if len(chi_list) == 0:
-                par_list.pop(0)
-                if len(par_list) > 0:
-                    chi_list = par_list[0].children_dirs
+        folder_dict = {}
+        cur_par = root
+        final_order.append(cur_par)
+        folder_dict[cur_par] = [len(cur_par.children_dirs), cur_par.children_dirs]
+        folders_left = folder_dict[cur_par][0]
+        count = 0
+        while folders_left != 0:
+                if folder_dict[cur_par][0] == 0:
+                    folder_dict[cur_par.parent_dir[0]][0] -= 1
+                print(count)
+                print(cur_par)
+                print(f"{cur_par} parent is {cur_par.parent_dir}")
+                print(f"{cur_par} children are {cur_par.children_dirs}")
+                print(folder_dict[cur_par][0])
+                print(final_order)
+                print(f"{folders_left} left")
+                if folder_dict[cur_par][0] != 0:
+                    cur_par = folder_dict[cur_par][1][len(folder_dict[cur_par][1]) - folder_dict[cur_par][0]]
+                    final_order.append(cur_par)
+                    folder_dict[cur_par] = [len(cur_par.children_dirs), cur_par.children_dirs]
+                else:
+                    cur_par = Builders.step_up(cur_par, folder_dict)
+                folders_left = 0
+                for v in folder_dict.values():
+                    folders_left += v[0]  
+                count += 1
                 
+
+                    
+            
+
+# Version 1
+            # for chi in cur_par.children_dirs:
+            
+            #     cur_par = chi
+            #     final_order.append(cur_par)
+            #     folder_dict[cur_par] = cur_par.children_dirs
+            #     folders_left = 0
+            #     breakpoint()
+            #     if cur_par in folder_dict[cur_par.parent_dir[0]]:
+            #         folder_dict[cur_par.parent_dir[0]].remove(cur_par)
+                
+            #     for v in folder_dict.values():
+            #         folders_left += len(v)    
+            #     if len(folder_dict[cur_par]) == 0:
+            #         Builders.step_up(cur_par, folder_dict)
+
+# Version 2
+        
+                # count = 0
+                # print(count)
+                # print(cur_par)
+                # print(f"{cur_par} parent is {cur_par.parent_dir}")
+                # print(f"{cur_par} children are {cur_par.children_dirs}")
+                # if len(cur_par.children_dirs) != 0:
+                #     cur_par = folder_dict[cur_par][0]
+                #     final_order.append(cur_par)
+                #     folder_dict[cur_par] = cur_par.children_dirs
+                #     folders_left = 0
+                #     if cur_par in folder_dict[cur_par.parent_dir[0]]:
+                #         cur_grand_par = cur_par.parent_dir[0]
+                #         folder_dict[cur_grand_par].remove(cur_par)
+                # else:
+                #     cur_par = Builders.step_up(cur_par, folder_dict)
+                # for v in folder_dict.values():
+                #     folders_left += len(v)    
+
+                # count += 1
                 
         return final_order
+    
+   
+    def step_up(cur_p, fol_dict):
+        
+        if len(fol_dict[cur_p]):
+            return cur_p.parent_dir[0]
