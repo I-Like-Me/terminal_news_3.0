98a3b3157fa45e1af11b721ca3984ebe082d1d56
I-Like-Me <cassgolkin@gmail.com>
2024-01-24 11:09:34 -0500

    back to simple

--- app/main/toolbox.py
+++ app/main/toolbox.py
@@ -72,131 +72,50 @@ class Builders:
                         arg_dict[k] = v
         table_name = Converters.str_to_class(f'{asset}')
         new_asset = table_name(**arg_dict)
-        return new_asset
+        return new_asset   
 
     def build_structure(root):
         step = 0
-        margin = 20
+        margin = 0
         final_order = []
-        item_dict = {}
+        folder_dict = {}
         name_dict = {}
         dict_tracker = []
-        cur_item = root
-        final_order.append(cur_item)
-        dict_tracker.append(cur_item.name)
-        item_dict[cur_item] = [len(cur_item.children_dirs), cur_item.children_dirs, len(cur_item.files), cur_item.files, margin]
-        name_dict[cur_item.name] = {'margin': margin}
-        items_left = item_dict[cur_item][0] + item_dict[cur_item][2]
-        loop = 0
-        # while items_left != 0:
-        #     print('--Start--')
-        #     print(f'Loop {loop}')
-        #     print(cur_item)
-        #     if type(cur_item) == Folder and item_dict[cur_item][0] == 0:
-        #         print('mark')
-        #         item_dict[cur_item.parent_dir[0]][0] -= 1
-        #     if type(cur_item) == File and item_dict[cur_item.parent_dir[0]][2] != 0:
-        #         print('kevin')
-        #         item_dict[cur_item.parent_dir[0]][2] -= 1
-        #     if type(cur_item) == Folder:
-        #         if item_dict[cur_item][0] != 0:
-        #             print('sue')
-        #             margin += 20
-        #             step += 1
-        #             cur_item = item_dict[cur_item][1][len(item_dict[cur_item][1]) - item_dict[cur_item][0]]
-        #             final_order.append(cur_item)
-        #             dict_tracker.append(cur_item.name)
-        #             Setter.set_folders(name_dict, dict_tracker, margin, 'folder')
-        #             item_dict[cur_item] = [len(cur_item.children_dirs), cur_item.children_dirs, len(cur_item.files), cur_item.files, margin]
-        #         elif item_dict[cur_item][0] == 0 and item_dict[cur_item][2] != 0:
-        #             print('stan')
-        #             cur_item = item_dict[cur_item][3][len(item_dict[cur_item][3]) - item_dict[cur_item][2]]
-        #             final_order.append(cur_item)
-        #             dict_tracker.append(cur_item.name)
-        #             Setter.set_folders(name_dict, dict_tracker, margin, 'file')
-        #             # item_dict[cur_item] = cur_item.name
-        #     else:
-        #         if type(cur_item) == File:
-        #             print('reese')
-        #             margin -= 20
-        #             step -= 1
-        #             dict_tracker.pop()
-        #             cur_item = cur_item.parent_dir[0]
-        #         if type(cur_item) == Folder:
-        #             while item_dict[cur_item][0] == 0:
-        #                 print('luke')
-        #                 margin -= 20
-        #                 step -= 1
-        #                 dict_tracker.pop()
-        #                 cur_item = cur_item.parent_dir[0]
-        #     items_left = 0
-        #     for v in item_dict.values():
-        #         print(v)
-        #         folders_left = v[0]
-        #         files_left = v[2] 
-        #         print(f'{folders_left} folders left and {files_left} files left')
-        #         both = folders_left + files_left
-        #         items_left += both
-        #     loop += 1
-        while items_left != 0:
-            print('--Start--')
-            # print(final_order)
-            print(name_dict)
-            print(cur_item)
-            # print(item_dict)
-            # print(items_left)
-            # print(name_dict)
-            if type(cur_item) == Folder:
-                # if item_dict[cur_item][0] == 0 and item_dict[cur_item][2] == 0:
-                #     item_dict[cur_item.parent_dir[0]][0] -= 1
-                if item_dict[cur_item][0] > 0:
-                    print('red')
-                    margin += 20
-                    step += 1
-                    cur_item = item_dict[cur_item][1][len(item_dict[cur_item][1]) - item_dict[cur_item][0]]
-                    final_order.append(cur_item)
-                    dict_tracker.append(cur_item.name)
-                    Setter.set_folders(name_dict, dict_tracker, margin)
-                    if cur_item not in item_dict:
-                        item_dict[cur_item] = [len(cur_item.children_dirs), cur_item.children_dirs, len(cur_item.files), cur_item.files, margin]
-                    if item_dict[cur_item][0] == 0 and item_dict[cur_item][2] == 0:
-                        item_dict[cur_item.parent_dir[0]][0] -= 1
-                elif item_dict[cur_item][0] == 0 and item_dict[cur_item][2] > 0:
-                    print('green')
-                    print(item_dict[cur_item][2])
-                    final_order.append(item_dict[cur_item][3][len(item_dict[cur_item][3]) - item_dict[cur_item][2]])
-                    dict_tracker.append(item_dict[cur_item][3][len(item_dict[cur_item][3]) - item_dict[cur_item][2]].name)
-                    Setter.set_folders(name_dict, dict_tracker, margin)
-                    print(item_dict[cur_item.parent_dir[0]][2])
-                    item_dict[cur_item][2] -= 1
-                else:
-                    print('blue')
-                    while item_dict[cur_item][0] == 0 and item_dict[cur_item][2] == 0:
-                        margin -= 20
-                        step -= 1
-                        dict_tracker.pop()
-                        cur_item = cur_item.parent_dir[0]
-                items_left = 0
-                for v in item_dict.values():
-                    items_left += v[0] + v[2]
+        cur_par = root
+        final_order.append(cur_par)
+        dict_tracker.append(cur_par)
+        folder_dict[cur_par] = [len(cur_par.children_dirs), cur_par.children_dirs, margin]
+        name_dict[cur_par] = {}
+        folders_left = folder_dict[cur_par][0]
+        while folders_left != 0:
+            if folder_dict[cur_par][0] == 0:
+                folder_dict[cur_par.parent_dir[0]][0] -= 1
+            if folder_dict[cur_par][0] != 0:
+                margin += 20
+                step += 1
+                cur_par = folder_dict[cur_par][1][len(folder_dict[cur_par][1]) - folder_dict[cur_par][0]]
+                final_order.append(cur_par)
+                dict_tracker.append(cur_par)
+                Setter.set_folders(name_dict, dict_tracker)
+                folder_dict[cur_par] = [len(cur_par.children_dirs), cur_par.children_dirs, margin]
+            else:
+                margin -= 20
+                step -= 1
+                dict_tracker.pop()
+                cur_par = cur_par.parent_dir[0]
+            folders_left = 0
+            for v in folder_dict.values():
+                folders_left += v[0]  
         return name_dict
     
+
 class Setter:
-    
-    def set_folders(dict_n, keys, mar):
-        for key in keys:
-            if key not in dict_n:
-                dict_n[key] = {'margin': mar}
-            dict_n = dict_n[key]
-        return dict_n
-    
-    def set_files(dict_n, keys):
-        print(dict_n)
-        for key in keys:
-            if key not in dict_n:
-                dict_n[key] = key
-            dict_n = dict_n[key]
-        return dict_n
 
-
+    def set_folders(dict_t, keys):
+        for key in keys:
+            if key not in dict_t:
+                dict_t[key] = {}
+            dict_t = dict_t[key]
+        dict_t.setdefault('Files', keys[-1].files)
+        return dict_t
     
