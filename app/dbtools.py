ada12154fbaaba7f8bb99786c16223b924360628
I-Like-Me <cassgolkin@gmail.com>
2024-04-08 10:32:54 -0400

    user creation fixed

--- app/dbtools.py
+++ app/dbtools.py
@@ -2,26 +2,10 @@ import json
 
 class JsonTools:
 
-    def find_path(json_obj, value, repr_p, path=()):
+    def find_path(json_obj, value, path=()):
         if isinstance(json_obj, dict):
             for k, v in json_obj.items():
-                if k == 'name':
-                    v_name = v
-                new_path = JsonTools.find_path(v, value, repr_p + (v_name,), path + (k,))
-                if new_path is not None:
-                    return new_path
-        elif isinstance(json_obj, list):
-            for i, v in enumerate(json_obj):
-                new_path = JsonTools.find_path(v, value, repr_p + (' > ',), path + (i,))
-                if new_path is not None:
-                    return new_path
-        elif json_obj == value:
-            return path, repr_p
-
-    def find_repr_path(json_obj, value, path=()):
-        if isinstance(json_obj, dict):
-            for k, v in json_obj.items():
-                new_path = JsonTools.find_path(v, value, path + (v,))
+                new_path = JsonTools.find_path(v, value, path + (k,))
                 if new_path is not None:
                     return new_path
         elif isinstance(json_obj, list):
@@ -31,15 +15,24 @@ class JsonTools:
                     return new_path
         elif json_obj == value:
             return path
+        
+    def build_repr_from_path(json_obj, path):
+        repr_path = []
+        for key in path:
+            if isinstance(key, int):
+                json_obj = json_obj[key]
+                repr_path.append(' > ')
+            else:
+                repr_path.append(json_obj.get('name'))
+                json_obj = json_obj.get(key)
+        return repr_path
 
     def path_to_string(path):
         return json.dumps(path)
     
-    def path_to_repr_string(path):
-        path_list = list(path)
-        path_string = path_list.pop(0)
-        for item in path_list:
-            path_string = path_string + f' > {item}'
+    def repr_path_to_string(path):
+        repr_str = ''.join(path) 
+        return repr_str
 
     def string_to_path(path_string):
         return json.loads(path_string)
